bc3c3c5c108d42ba4726c55b6d5ff9fb8d24880b
Arjun M <98508543+Amg1703@users.noreply.github.com>
2025-09-05 01:07:24 +0530

    Adding test file directly since test file was committed but code wasnt uploaded by github

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_create_blog():
+    response = client.post("/blog/", json={"blog_title": "Test Blog", "blog_body": "Test Content"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["blog_title"] == "Test Blog"
+    assert data["blog_body"] == "Test Content"
+
+def test_get_all_blogs():
+    response = client.get("/getallblogs/")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_blogs_by_wrong_id():
+    response = client.get("/getblogsbyid/99999")
+    assert response.status_code == 404
