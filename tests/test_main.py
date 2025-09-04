143ad062f9fe815764465036fb0f42121a7333e9
Arjun M <98508543+Amg1703@users.noreply.github.com>
2025-09-05 01:10:25 +0530

    Adding system path since tests cannot access main folder from the root

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import app
 
 client = TestClient(app)
@@ -18,3 +20,4 @@ def test_get_all_blogs():
 def test_get_blogs_by_wrong_id():
     response = client.get("/getblogsbyid/99999")
     assert response.status_code == 404
+
